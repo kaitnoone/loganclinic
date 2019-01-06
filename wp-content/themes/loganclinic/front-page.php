b7d89b35a4fdd8b07b2e6887576a3c0cc32a78cb
Kaitlyn Noone <kait.noone@gmail.com>
2019-01-06 14:45:00 -0600

    template and style changes to add video to homepage

--- wp-content/themes/loganclinic/front-page.php
+++ wp-content/themes/loganclinic/front-page.php
@@ -7,8 +7,9 @@
 get_header();
 ?>
 
-<div class="hero--home" style="background-image: url('<?php the_field("home_hero_image");?>')">
-    <h1 class="hero--home_headline"><span><?php the_field('home_hero_headline');?></span></h1>
+<div class="hero--home" style="background-image: url('<?php the_field(" home_hero_image");?>')">
+    <h1 class="hero--home_headline"><span>
+            <?php the_field('home_hero_headline');?></span></h1>
 </div>
 
 <div class="subscribe">
@@ -25,37 +26,43 @@ get_header();
 </div>
 
 <div class="cta">
-	<div class="container">
-        <h2 class="cta--title"><?php the_field('cta_headline');?></h2>
-        <div class="cta--content"><?php the_field('cta_content');?></div>
+    <div class="container">
+        <h2 class="cta--title">
+            <?php the_field('cta_headline');?>
+        </h2>
+        <div class="cta--content">
+            <?php the_field('cta_content');?>
+        </div>
         <a class="button" href="/about">Learn More</a>
 
     </div>
 </div>
 
 <div class="offset-container" style="background-image: url('<?php the_field('offset-background_image');?>')">
-	<div class="container">
+    <div class="container">
         <div class="content">
-            <h3><?php the_field('offset-headline');?></h3>
+            <h3>
+                <?php the_field('offset-headline');?>
+            </h3>
             <?php the_field('offset-content');?>
         </div>
     </div>
 </div>
 
-<div class="about-providers container--left-image">
-	<div class="container">
-        <h3 class="headline"><?php the_field('image-left-headline');?></h3>
+<div class="video--full-width">
+    <div class="container">
         <div class="wrap">
-            <div class="image">
-                <img src="<?php the_field('image-left-image');?>" alt="<?php the_field('image-alt-text');?>" />
+            <h3>Patient Care</h3>
+            <div class="video">
+                <iframe width="100%" height="500" src="<?php the_field('video_embed_link');?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
             </div>
-            <div class="content"><?php the_field('image-left-content');?></div>
         </div>
     </div>
 </div>
 
 <div class="membership-levels">
-	<div class="container">
+    <div class="container">
         <h3 class="headline">Monthly Membership</h3>
         <div class="table">
             <table class="membership-levels--info" cellpadding="0" cellspacing="0">
@@ -68,19 +75,27 @@ get_header();
                 <tbody>
                     <tr>
                         <td>0-18 Years</td>
-                        <td><?php the_field('0-18_years');?></td>
+                        <td>
+                            <?php the_field('0-18_years');?>
+                        </td>
                     </tr>
                     <tr>
                         <td>19-44 Years</td>
-                        <td><?php the_field('19-44_years');?></td>
+                        <td>
+                            <?php the_field('19-44_years');?>
+                        </td>
                     </tr>
                     <tr>
                         <td>45-64 Years</td>
-                        <td><?php the_field('45-64_years');?></td>
+                        <td>
+                            <?php the_field('45-64_years');?>
+                        </td>
                     </tr>
                     <tr>
                         <td>65+ Years</td>
-                        <td><?php the_field('65_years');?></td>
+                        <td>
+                            <?php the_field('65_years');?>
+                        </td>
                     </tr>
                 </tbody>
             </table>
